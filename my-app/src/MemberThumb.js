4ea2a095e74da98589ac71fa7a6c8c21d1f08942
carinutza <ci2106@nyu.edu>
2025-10-28 17:59:57 +0100

    added kick button

--- my-app/src/MemberThumb.js
+++ my-app/src/MemberThumb.js
@@ -2,18 +2,26 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './MemberThumb.css'
 
+
 const MemberThumb = props => {
   // inject a random placeholder image if avatar missing
   const imgSrc = props.details.avatar || `https://i.pravatar.cc/100?u=${props.details.id}`
 
   return (
     <article className="MemberThumb">
-      <Link to={`/members/${props.details.id}`}>
-        <img alt={`${props.details.first_name} ${props.details.last_name}`} src={imgSrc} /> </Link>
+        <img alt={`${props.details.first_name} ${props.details.last_name}`} src={imgSrc} />
         <h2>{props.details.first_name} {props.details.last_name}</h2>
         <div className="username">@{props.details.username}</div>
         <div className="country">From: {props.details.country}</div>
         <p className="description">Interests: {props.details.description}</p>
+        {props.canKick && (
+        <button
+             className="kick-button"
+             onClick={() => alert(`You just kicked ${props.details.first_name} (pretend)!`)}
+        >
+            Kick Member
+        </button>
+        )}
     </article>
   )
 }
