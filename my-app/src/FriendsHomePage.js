56da2c3891d89351fa49b4c831355132784cfe3a
Lovnish2145 <julkalovnish@gmail.com>
2025-10-28 16:43:00 -0400

    feat: Add Page Header and Layout #127

--- my-app/src/FriendsHomePage.js
+++ my-app/src/FriendsHomePage.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./FriendsHomePage.css";
+import Logo from "./logo.svg"; // Import the logo
 
-function FriendsHomePage() {
+const FriendsHomePage = () => {
   return (
-    <div className="friends-home">
+    <div className="FriendsHomePage">
+      <div className="FriendsHomePage-Logotext">
+        <div className="FriendsHomePage-textLogoText">
+          <img src={Logo} alt="App Logo" className="app-logo" />
+        </div>
+      </div>
       <h1>Friends Home Page</h1>
+      <p><i>Here you can manage your friends, requests, and more!</i></p>
     </div>
   );
-}
+};
 
 export default FriendsHomePage;
