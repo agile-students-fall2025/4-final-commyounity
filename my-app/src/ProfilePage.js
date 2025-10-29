837bb676efafc5fefc8f5f2231a1a426e977e6c3
V-run64 <vpandian4u@gmail.com>
2025-10-29 10:40:53 -0400

    profile page routing fix
    fixed profile page back button, goes back to last page rather than viewboards

--- my-app/src/ProfilePage.js
+++ my-app/src/ProfilePage.js
@@ -21,7 +21,7 @@ export default function ProfilePage() {
         {/* The back button. Right now it just links to /viewboards as a placeholder. */}
         <button
           className="back-button"
-          onClick={() => navigate("/viewboards")}
+          onClick={() => navigate(-1)}
         >
           ←
         </button>
