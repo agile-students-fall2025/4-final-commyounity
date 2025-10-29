fca73a66b5809c94d72bc0161cb4ff4bb80d1751
carinutza <ci2106@nyu.edu>
2025-10-29 13:25:45 +0100

    added back button to browse

--- my-app/src/BrowseBoard.js
+++ my-app/src/BrowseBoard.js
@@ -1,11 +1,21 @@
 import Logo from "./logo.svg"
 import "./BrowseBoard.css"
 import BrowseBoardList from "./BrowseBoardList";
+import { useNavigate } from "react-router-dom";
 
 const BrowseBoard = () => {
+    const navigate = useNavigate();
     console.log("**")
     return <>
     <Logotext/>
+    {/* Back to Home button */}
+    <button
+        className="back-home-btn"
+        onClick={() => navigate("/home")}
+        style={{ marginTop: "20px" }}
+      >
+        ← Back to Home
+      </button>
     <div className="browse-board-content">
         <BrowseBoardList /> 
     </div>
