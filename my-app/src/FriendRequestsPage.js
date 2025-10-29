eec57c9295d71493c685cb88ce115fff4ecb9edb
Lovnish2145 <julkalovnish@gmail.com>
2025-10-29 01:10:09 -0400

    feat: Set up Friend Requests page and route #109

--- my-app/src/FriendRequestsPage.js
+++ my-app/src/FriendRequestsPage.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./FriendRequestsPage.css";
+import Logo from "./logo.svg";
 
 const FriendRequestsPage = () => {
   return (
-    <div style={{ textAlign: "center", marginTop: "80px" }}>
-      <h1>Friend Requests Page</h1>
-      <p>This is where pending requests will appear.</p>
+    <div className="FriendRequestsPage">
+      <header className="friendrequests-header">
+        <div className="friendrequests-logo">
+          <img src={Logo} alt="App logo" />
+        </div>
+        <Link to="/friends" className="back-btn">
+          ← Back to Friends Home
+        </Link>
+      </header>
+
+      <h1>Friend Requests</h1>
+      <p>
+        <i>Review and manage people who want to connect with you.</i>
+      </p>
     </div>
   );
 };
