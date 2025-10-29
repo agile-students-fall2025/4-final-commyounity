5394799d16849b317cdb410c6b41780e90479036
Lovnish2145 <julkalovnish@gmail.com>
2025-10-28 21:59:09 -0400

    feat: Create Friends List page and route #93

--- my-app/src/FriendsListPage.js
+++ my-app/src/FriendsListPage.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./FriendsListPage.css";
+import Logo from "./logo.svg";
 
-const FriendsListPage = () => {
+const FriendsList = () => {
   return (
-    <div style={{ textAlign: "center", marginTop: "80px" }}>
-      <h1>Friends List Page</h1>
-      <p>This is where the user’s friends will be displayed.</p>
+    <div className="FriendsList">
+      <header className="friendslist-header">
+        {/* App logo */}
+        <div className="friendslist-logo">
+          <img src={Logo} alt="App logo" />
+        </div>
+
+        {/* Back button to main friends page */}
+        <Link to="/friends" className="back-btn">
+          ← Back to Friends Home
+        </Link>
+      </header>
+
+      <h1>Friends List </h1>
+      <p><i>See your friends!</i></p>
     </div>
   );
 };
 
-export default FriendsListPage;
+export default FriendsList;
