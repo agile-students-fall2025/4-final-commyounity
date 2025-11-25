3f9337196df790f57127552b2e66809a44da5841
Leo <fuleo2400@gmail.com>
2025-11-25 23:48:06 +0400

    UserDataBase passwordUpdate

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const bcrypt = require('bcryptjs')
 
 const userSchema = new Schema(
   {
@@ -68,6 +69,29 @@ const userSchema = new Schema(
 userSchema.index({ email: 1 });
 userSchema.index({ username: 1 });
 userSchema.index({ googleId: 1 });
+
+// hash the password before the user is saved
+// mongoose provides hooks that allow us to run code before or after specific events
+userSchema.pre('save', function (next) {
+  const user = this
+  // if the password has not changed, no need to hash it
+  if (!user.isModified('password')) return next()
+  // if user is from Google OAuth and has no password, skip hashing
+  if (user.googleId && !user.password) return next()
+  // otherwise, the password is being modified, so hash it
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    if (err) return next(err)
+    user.password = hash // update the password to the hashed version
+    next()
+  })
+})
+
+// mongoose allows us to attach methods to a model...
+
+// compare a given password with the database hash
+userSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.password)
+}
 
 module.exports = mongoose.model("User", userSchema);
 
